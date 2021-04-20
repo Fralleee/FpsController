a5f10214b85c116cf834fb055883ceb36934bdd7
Roland Chelwing <jolleee@hotmail.com>
2021-04-20 11:32:50 +0200

    Naming

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -14,12 +14,12 @@ namespace Fralle.FpsController
 		public event Action<bool> OnCrouchStateChanged = delegate { };
 
 		[Header("Setup")]
-		public new Camera camera;
-		public Transform cameraRig;
-		public Transform orientation;
-		public Transform body;
-		[HideInInspector] public Rigidbody rigidBody;
-		[HideInInspector] public CapsuleCollider capsule;
+		public Camera Camera;
+		public Transform CameraRig;
+		public Transform Orientation;
+		public Transform Body;
+		[HideInInspector] public Rigidbody RigidBody;
+		[HideInInspector] public CapsuleCollider Capsule;
 
 		[Header("Flags")]
 		[Readonly] public bool IsLocked;
@@ -37,7 +37,7 @@ namespace Fralle.FpsController
 		[SerializeField] float baseMovementSpeed = 4f;
 		[SerializeField] float stopTime = 0.05f;
 		[SerializeField] float airControl = 0.5f;
-		[Readonly] public float modifiedMovementSpeed;
+		[Readonly] public float ModifiedMovementSpeed;
 
 		[Header("Step Climbing")]
 		[SerializeField] float stepHeight = 0.4f;
@@ -47,21 +47,20 @@ namespace Fralle.FpsController
 		[SerializeField] float maxSlopeGlideAngle = 35;
 		[SerializeField] float maxWalkableSlopeAngle = 45;
 		[SerializeField] float groundCheckDistance = 0.1f;
-		[SerializeField] float stickToGroundHelperDistance = 0.5f; // stops the character
 
 		[Header("Jump")]
 		[SerializeField] float baseJumpStrength = 8f;
 		[SerializeField] float fallMultiplier = 2.5f;
 		[SerializeField] float lowJumpModifier = 2f;
-		[Readonly] public float modifiedJumpStrength;
+		[Readonly] public float ModifiedJumpStrength;
 
 		[Header("Crouching")]
 		[SerializeField] float crouchingSpeed = 8f;
 		[SerializeField] float crouchHeight = 1f;
 
 		readonly List<ContactPoint> allCPs = new List<ContactPoint>();
-		public Vector2 movement { get; private set; }
-		public Vector2 mouseLook { get; private set; }
+		public Vector2 Movement { get; private set; }
+		public Vector2 MouseLook { get; private set; }
 		PlayerInput playerInput;
 		Vector3 lastVelocity;
 		Vector3 desiredForce;
@@ -82,23 +81,23 @@ namespace Fralle.FpsController
 		float roofCheckHeight;
 		float slopeMultiplier = 1f;
 		readonly float slopeGlideMax = 100f;
-		[Readonly] public float slopeAngle;
-		float actualGroundCheckDistance => (capsule.height / 2f) - capsule.radius + groundCheckDistance;
+		[Readonly] public float SlopeAngle;
+		float ActualGroundCheckDistance => (Capsule.height / 2f) - Capsule.radius + groundCheckDistance;
 
 		void Awake()
 		{
-			rigidBody = body.GetComponent<Rigidbody>();
-			capsule = body.GetComponent<CapsuleCollider>();
+			RigidBody = Body.GetComponent<Rigidbody>();
+			Capsule = Body.GetComponent<CapsuleCollider>();
 
-			defaultScale = body.localScale;
+			defaultScale = Body.localScale;
 			crouchingScale = new Vector3(1, 0.5f, 1);
-			crouchHeight = capsule.height * crouchingScale.y * body.localScale.y;
-			roofCheckHeight = capsule.height - crouchHeight * 0.5f - 0.01f;
+			crouchHeight = Capsule.height * crouchingScale.y * Body.localScale.y;
+			roofCheckHeight = Capsule.height - crouchHeight * 0.5f - 0.01f;
 
-			ConfigureCursor(true);
+			ConfigureCursor();
 
-			if (!camera)
-				camera = Camera.main;
+			if (!Camera)
+				Camera = Camera.main;
 
 			playerInput = GetComponent<PlayerInput>();
 			playerInput.actions["Movement"].performed += OnMovement;
@@ -108,8 +107,8 @@ namespace Fralle.FpsController
 			playerInput.actions["Crouch"].canceled += OnCrouchCancel;
 			playerInput.actions["Crouch"].performed += OnCrouch;
 
-			modifiedMovementSpeed = baseMovementSpeed;
-			modifiedJumpStrength = baseJumpStrength;
+			ModifiedMovementSpeed = baseMovementSpeed;
+			ModifiedJumpStrength = baseJumpStrength;
 		}
 
 		void Update()
@@ -127,8 +126,8 @@ namespace Fralle.FpsController
 		{
 			if (!playerInput.inputIsActive)
 			{
-				mouseLook = Vector2.zero;
-				movement = Vector2.zero;
+				MouseLook = Vector2.zero;
+				Movement = Vector2.zero;
 				jumpButton = false;
 				crouchButton = false;
 			}
@@ -136,7 +135,7 @@ namespace Fralle.FpsController
 			if (IsLocked)
 				return;
 
-			desiredForce = orientation.right * movement.x + orientation.forward * movement.y;
+			desiredForce = Orientation.right * Movement.x + Orientation.forward * Movement.y;
 			GroundedCheck();
 
 			if (IsGrounded)
@@ -165,12 +164,12 @@ namespace Fralle.FpsController
 		#region Input
 		public void OnMovement(InputAction.CallbackContext context)
 		{
-			movement = context.ReadValue<Vector2>();
+			Movement = context.ReadValue<Vector2>();
 		}
 
 		public void OnLook(InputAction.CallbackContext context)
 		{
-			mouseLook = context.ReadValue<Vector2>();
+			MouseLook = context.ReadValue<Vector2>();
 		}
 
 		public void OnJumpCancel(InputAction.CallbackContext context)
@@ -195,18 +194,18 @@ namespace Fralle.FpsController
 		#region Camera Control
 		void CameraLook()
 		{
-			mouseCoords.y += mouseLook.y * mouseSensitivity;
+			mouseCoords.y += MouseLook.y * mouseSensitivity;
 			mouseCoords.y = Mathf.Clamp(mouseCoords.y, -clampY, clampY);
-			mouseCoords.x += mouseLook.x * mouseSensitivity;
+			mouseCoords.x += MouseLook.x * mouseSensitivity;
 
 			currentRotationX = Mathf.SmoothDamp(currentRotationX, mouseCoords.x + affectRotation.x, ref mouseLookDampX, smoothTime);
 			currentRotationY = Mathf.SmoothDamp(currentRotationY, mouseCoords.y + affectRotation.y, ref mouseLookDampY, smoothTime);
 
 			affectRotation = Vector2.SmoothDamp(affectRotation, Vector2.zero, ref affectRotation, 0);
 
-			var rot = orientation.transform.rotation.eulerAngles;
-			orientation.transform.localRotation = Quaternion.Euler(rot.x, currentRotationX, rot.z);
-			cameraRig.localRotation = Quaternion.Euler(currentRotationY, currentRotationX, 0);
+			var rot = Orientation.transform.rotation.eulerAngles;
+			Orientation.transform.localRotation = Quaternion.Euler(rot.x, currentRotationX, rot.z);
+			CameraRig.localRotation = Quaternion.Euler(currentRotationY, currentRotationX, 0);
 		}
 
 		public static void ConfigureCursor(bool doLock = true)
@@ -228,12 +227,12 @@ namespace Fralle.FpsController
 		{
 			previouslyGrounded = IsGrounded;
 
-			Debug.DrawLine(body.position + capsule.center, body.position + capsule.center + Vector3.down * (actualGroundCheckDistance + capsule.radius - 0.001f));
+			Debug.DrawLine(Body.position + Capsule.center, Body.position + Capsule.center + Vector3.down * (ActualGroundCheckDistance + Capsule.radius - 0.001f));
 
-			if (Physics.SphereCast(body.position + capsule.center, capsule.radius - 0.001f, Vector3.down, out RaycastHit hitInfo, actualGroundCheckDistance, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+			if (Physics.SphereCast(Body.position + Capsule.center, Capsule.radius - 0.001f, Vector3.down, out RaycastHit hitInfo, ActualGroundCheckDistance, Physics.AllLayers, QueryTriggerInteraction.Ignore))
 			{
-				if (Physics.Raycast(body.position + capsule.center, Vector3.down, out RaycastHit raycastHit,
-					(actualGroundCheckDistance + capsule.radius - 0.001f), Physics.AllLayers, QueryTriggerInteraction.Ignore))
+				if (Physics.Raycast(Body.position + Capsule.center, Vector3.down, out RaycastHit raycastHit,
+					(ActualGroundCheckDistance + Capsule.radius - 0.001f), Physics.AllLayers, QueryTriggerInteraction.Ignore))
 				{
 					groundContactNormal = raycastHit.normal;
 				}
@@ -249,12 +248,12 @@ namespace Fralle.FpsController
 				groundContactNormal = Vector3.up;
 			}
 
-			rigidBody.useGravity = !IsGrounded;
+			RigidBody.useGravity = !IsGrounded;
 			if (previouslyGrounded == IsGrounded)
 				return;
 			if (IsGrounded)
 			{
-				OnGroundEnter(rigidBody.velocity.y);
+				OnGroundEnter(RigidBody.velocity.y);
 				IsJumping = false;
 				jumpButton = false;
 			}
@@ -264,12 +263,12 @@ namespace Fralle.FpsController
 
 		void SlopeControl()
 		{
-			slopeAngle = Vector3.Angle(groundContactNormal, Vector3.up);
+			SlopeAngle = Vector3.Angle(groundContactNormal, Vector3.up);
 
-			if (Physics.Raycast(orientation.position, desiredForce, out RaycastHit raycastHit, capsule.radius,
+			if (Physics.Raycast(Orientation.position, desiredForce, out RaycastHit raycastHit, Capsule.radius,
 				Physics.AllLayers, QueryTriggerInteraction.Ignore))
 			{
-				var forwardSlopeAngle = Vector3.Angle(raycastHit.normal, Vector3.up);
+				float forwardSlopeAngle = Vector3.Angle(raycastHit.normal, Vector3.up);
 
 				if (forwardSlopeAngle >= 89) // not a slope
 					slopeMultiplier = 1f;
@@ -284,27 +283,27 @@ namespace Fralle.FpsController
 			}
 
 
-			if (slopeAngle <= 0)
+			if (SlopeAngle <= 0)
 				return;
 
-			if (slopeAngle > maxWalkableSlopeAngle + 1)
+			if (SlopeAngle > maxWalkableSlopeAngle + 1)
 			{
-				rigidBody.AddForce(Vector3.down * slopeGlideMax, ForceMode.Acceleration);
+				RigidBody.AddForce(Vector3.down * slopeGlideMax, ForceMode.Acceleration);
 				return;
 			}
 
-			if (slopeAngle > maxSlopeGlideAngle + 1f)
+			if (SlopeAngle > maxSlopeGlideAngle + 1f)
 			{
-				var factor = slopeAngle / maxWalkableSlopeAngle;
-				rigidBody.AddForce(Vector3.down * slopeGlideMax * factor, ForceMode.Acceleration);
+				float factor = SlopeAngle / maxWalkableSlopeAngle;
+				RigidBody.AddForce(Vector3.down * slopeGlideMax * factor, ForceMode.Acceleration);
 				return;
 			}
 
-			if (!(rigidBody.velocity.y >= -0.2f))
+			if (!(RigidBody.velocity.y >= -0.2f))
 				return;
 
-			rigidBody.useGravity = false;
-			rigidBody.AddForce(-groundContactNormal * 150f);
+			RigidBody.useGravity = false;
+			RigidBody.AddForce(-groundContactNormal * 150f);
 		}
 
 		void StickToGroundHelper()
@@ -312,7 +311,7 @@ namespace Fralle.FpsController
 			if (Mathf.Abs(Vector3.Angle(groundContactNormal, Vector3.up)) >= maxWalkableSlopeAngle)
 				return;
 
-			rigidBody.velocity = Vector3.ProjectOnPlane(rigidBody.velocity, groundContactNormal);
+			RigidBody.velocity = Vector3.ProjectOnPlane(RigidBody.velocity, groundContactNormal);
 		}
 		#endregion
 		#region Gravity Adjustments
@@ -321,10 +320,10 @@ namespace Fralle.FpsController
 			if (IsGrounded)
 				return;
 
-			if (rigidBody.velocity.y < 0)
-				rigidBody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
-			else if (rigidBody.velocity.y > 0 && !jumpButton)
-				rigidBody.velocity += Vector3.up * Physics.gravity.y * (lowJumpModifier - 1) * Time.fixedDeltaTime;
+			if (RigidBody.velocity.y < 0)
+				RigidBody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+			else if (RigidBody.velocity.y > 0 && !jumpButton)
+				RigidBody.velocity += Vector3.up * Physics.gravity.y * (lowJumpModifier - 1) * Time.fixedDeltaTime;
 		}
 		#endregion
 		#region Movement
@@ -344,39 +343,39 @@ namespace Fralle.FpsController
 		void GroundMove()
 		{
 			desiredForce = Vector3.ProjectOnPlane(desiredForce, groundContactNormal).normalized;
-			rigidBody.AddForce(desiredForce * modifiedMovementSpeed * slopeMultiplier, ForceMode.Impulse);
+			RigidBody.AddForce(desiredForce * ModifiedMovementSpeed * slopeMultiplier, ForceMode.Impulse);
 			StoppingForcesGround();
 		}
 
 		void AirMove()
 		{
-			rigidBody.AddForce(desiredForce * modifiedMovementSpeed * airControl, ForceMode.Impulse);
+			RigidBody.AddForce(desiredForce * ModifiedMovementSpeed * airControl, ForceMode.Impulse);
 			StoppingForcesAir();
 		}
 
 		void StoppingForcesGround()
 		{
-			rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, Vector3.zero, ref damp, stopTime);
+			RigidBody.velocity = Vector3.SmoothDamp(RigidBody.velocity, Vector3.zero, ref damp, stopTime);
 		}
 		void StoppingForcesAir()
 		{
-			rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, Vector3.zero, ref damp, stopTime).With(y: rigidBody.velocity.y);
+			RigidBody.velocity = Vector3.SmoothDamp(RigidBody.velocity, Vector3.zero, ref damp, stopTime).With(y: RigidBody.velocity.y);
 		}
 		#endregion
 		#region Step Climbing
 		void ClimbSteps()
 		{
-			Vector3 velocity = rigidBody.velocity;
-			var grounded = FindGround(out ContactPoint groundCP, allCPs);
+			Vector3 velocity = RigidBody.velocity;
+			bool grounded = FindGround(out ContactPoint groundCp, allCPs);
 			Vector3 stepUpOffset = default;
 			bool stepUp = false;
 			if (grounded)
-				stepUp = FindStep(out stepUpOffset, allCPs, groundCP, velocity);
+				stepUp = FindStep(out stepUpOffset, allCPs, groundCp, velocity);
 
 			if (stepUp)
 			{
-				rigidBody.position += stepUpOffset;
-				rigidBody.velocity = lastVelocity;
+				RigidBody.position += stepUpOffset;
+				RigidBody.velocity = lastVelocity;
 			}
 
 			allCPs.Clear();
@@ -384,64 +383,62 @@ namespace Fralle.FpsController
 		}
 
 
-		bool FindGround(out ContactPoint groundCP, List<ContactPoint> allCPs)
+		static bool FindGround(out ContactPoint groundCp, IEnumerable<ContactPoint> allCPs)
 		{
-			groundCP = default;
-			var found = false;
+			groundCp = default;
+			bool found = false;
 			foreach (ContactPoint cp in allCPs)
 			{
-				if (cp.normal.y > 0.0001f && (!found || cp.normal.y > groundCP.normal.y))
-				{
-					groundCP = cp;
-					found = true;
-				}
+				if (!(cp.normal.y > 0.0001f) || (found && !(cp.normal.y > groundCp.normal.y))) continue;
+				groundCp = cp;
+				found = true;
 			}
 
 			return found;
 		}
 
-		bool FindStep(out Vector3 stepUpOffset, List<ContactPoint> allCPs, ContactPoint groundCP, Vector3 currVelocity)
+		bool FindStep(out Vector3 stepUpOffset, IEnumerable<ContactPoint> allContactPoints, ContactPoint groundCp, Vector3 currVelocity)
 		{
 			stepUpOffset = default;
-			Vector2 velocityXZ = new Vector2(currVelocity.x, currVelocity.z);
-			if (velocityXZ.sqrMagnitude < 0.0001f)
+			Vector2 velocityXz = new Vector2(currVelocity.x, currVelocity.z);
+			if (velocityXz.sqrMagnitude < 0.0001f)
 				return false;
 
-			foreach (ContactPoint cp in allCPs)
+			foreach (ContactPoint cp in allContactPoints)
 			{
-				bool test = ResolveStepUp(out stepUpOffset, cp, groundCP);
+				bool test = ResolveStepUp(out stepUpOffset, cp, groundCp);
 				if (test)
-					return test;
+					return true;
 			}
 			return false;
 		}
 
-		bool ResolveStepUp(out Vector3 stepUpOffset, ContactPoint stepTestCP, ContactPoint groundCP)
+		bool ResolveStepUp(out Vector3 stepUpOffset, ContactPoint stepTestCp, ContactPoint groundCp)
 		{
 			stepUpOffset = default;
-			Collider stepCol = stepTestCP.otherCollider;
+			Collider stepCol = stepTestCp.otherCollider;
 
-			if (Mathf.Abs(stepTestCP.normal.y) >= 0.01f)
+			if (Mathf.Abs(stepTestCp.normal.y) >= 0.01f)
 			{
 				return false;
 			}
 
-			if (stepTestCP.point.y - groundCP.point.y >= stepHeight)
+			if (stepTestCp.point.y - groundCp.point.y >= stepHeight)
 			{
 				return false;
 			}
 
-			float actualStepHeight = groundCP.point.y + stepHeight + 0.0001f;
-			Vector3 stepTestInvDir = new Vector3(-stepTestCP.normal.x, 0, -stepTestCP.normal.z).normalized;
-			Vector3 origin = new Vector3(stepTestCP.point.x, actualStepHeight, stepTestCP.point.z) + (stepTestInvDir * stepSearchOvershoot);
+			float actualStepHeight = groundCp.point.y + stepHeight + 0.0001f;
+			Vector3 stepTestInvDir = new Vector3(-stepTestCp.normal.x, 0, -stepTestCp.normal.z).normalized;
+			Vector3 origin = new Vector3(stepTestCp.point.x, actualStepHeight, stepTestCp.point.z) + (stepTestInvDir * stepSearchOvershoot);
 			Vector3 direction = Vector3.down;
 			if (!(stepCol.Raycast(new Ray(origin, direction), out RaycastHit hitInfo, stepHeight)))
 			{
 				return false;
 			}
 
-			Vector3 stepUpPoint = new Vector3(stepTestCP.point.x, hitInfo.point.y + 0.0001f, stepTestCP.point.z) + (stepTestInvDir * stepSearchOvershoot);
-			Vector3 stepUpPointOffset = stepUpPoint - new Vector3(stepTestCP.point.x, groundCP.point.y, stepTestCP.point.z);
+			Vector3 stepUpPoint = new Vector3(stepTestCp.point.x, hitInfo.point.y + 0.0001f, stepTestCp.point.z) + (stepTestInvDir * stepSearchOvershoot);
+			Vector3 stepUpPointOffset = stepUpPoint - new Vector3(stepTestCp.point.x, groundCp.point.y, stepTestCp.point.z);
 
 			stepUpOffset = stepUpPointOffset;
 			return true;
@@ -457,9 +454,9 @@ namespace Fralle.FpsController
 					OnCrouchStateChanged(true);
 
 				IsCrouching = true;
-				if (body.localScale != crouchingScale)
+				if (Body.localScale != crouchingScale)
 				{
-					body.localScale = Vector3.Lerp(body.localScale, crouchingScale, Time.deltaTime * crouchingSpeed);
+					Body.localScale = Vector3.Lerp(Body.localScale, crouchingScale, Time.deltaTime * crouchingSpeed);
 				}
 			}
 			else if (IsCrouching && !Physics.Raycast(transform.position, Vector3.up, roofCheckHeight))
@@ -468,9 +465,9 @@ namespace Fralle.FpsController
 				OnCrouchStateChanged(false);
 			}
 
-			if (!IsCrouching && body.localScale != defaultScale)
+			if (!IsCrouching && Body.localScale != defaultScale)
 			{
-				body.localScale = Vector3.Lerp(body.localScale, defaultScale, Time.deltaTime * crouchingSpeed);
+				Body.localScale = Vector3.Lerp(Body.localScale, defaultScale, Time.deltaTime * crouchingSpeed);
 			}
 		}
 		#endregion
@@ -483,20 +480,20 @@ namespace Fralle.FpsController
 			queueJump = false;
 			IsJumping = true;
 			IsGrounded = false;
-			rigidBody.useGravity = true;
-			rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0f, rigidBody.velocity.z);
-			rigidBody.AddForce(Vector3.up * modifiedJumpStrength * slopeMultiplier, ForceMode.VelocityChange);
+			RigidBody.useGravity = true;
+			RigidBody.velocity = new Vector3(RigidBody.velocity.x, 0f, RigidBody.velocity.z);
+			RigidBody.AddForce(Vector3.up * ModifiedJumpStrength * slopeMultiplier, ForceMode.VelocityChange);
 		}
 		#endregion
 		#region Limit speed
 		void LimitSpeed()
 		{
-			var horizontalMovement = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z);
-			if (horizontalMovement.magnitude <= modifiedMovementSpeed)
+			var horizontalMovement = new Vector3(RigidBody.velocity.x, 0, RigidBody.velocity.z);
+			if (horizontalMovement.magnitude <= ModifiedMovementSpeed)
 				return;
 
-			horizontalMovement = horizontalMovement.normalized * modifiedMovementSpeed;
-			rigidBody.velocity = new Vector3(horizontalMovement.x, rigidBody.velocity.y, horizontalMovement.z);
+			horizontalMovement = horizontalMovement.normalized * ModifiedMovementSpeed;
+			RigidBody.velocity = new Vector3(horizontalMovement.x, RigidBody.velocity.y, horizontalMovement.z);
 		}
 		#endregion
 
