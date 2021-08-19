6d1e3223d27580d78d2bbecc9033c385149a4de7
Roland Chelwing <jolleee@hotmail.com>
2021-08-19 15:08:58 +0200

    Update GroundCheck.cs

--- Scripts/Monobehaviours/GroundCheck.cs
+++ Scripts/Monobehaviours/GroundCheck.cs
@@ -6,6 +6,8 @@ public class GroundCheck : MonoBehaviour
   RigidbodyController rigidbodyController;
   CapsuleCollider capsuleCollider;
 
+  ContactPoint[] contacts;
+
   Vector3 bottom => capsuleCollider.bounds.center - (Vector3.up * capsuleCollider.bounds.extents.y);
   Vector3 curve => bottom + (Vector3.up * capsuleCollider.radius * 0.5f);
 
@@ -20,19 +22,28 @@ public class GroundCheck : MonoBehaviour
     if (rigidbodyController.IsJumping)
       return;
 
-    rigidbodyController.ResetGroundVariables();
-
-    var contacts = collision.contacts;
+    contacts = collision.contacts;
     foreach (ContactPoint contactPoint in contacts)
     {
       Vector3 dir = curve - contactPoint.point;
+
       if (dir.y > 0f)
       {
         rigidbodyController.GroundContactNormal = contactPoint.normal;
         rigidbodyController.SlopeAngle = Mathf.Min(rigidbodyController.SlopeAngle, Vector3.Angle(contactPoint.normal, Vector3.up));
         rigidbodyController.IsGrounded = true;
       }
-
     }
   }
+
+  //void OnDrawGizmosSelected()
+  //{
+  //  Gizmos.color = Color.blue;
+  //  foreach (ContactPoint contactPoint in contacts)
+  //    Gizmos.DrawSphere(contactPoint.point, .25f);
+
+  //  Gizmos.color = Color.red;
+  //  Gizmos.DrawSphere(bottom, .05f);
+  //  Gizmos.DrawSphere(curve, .05f);
+  //}
 }
